92b36006958cde4ff321b4af6701419fa0cd9a8a
Dan Høegh <dan.hoegh@gmail.com>
2020-01-03 13:13:38 +0100

    Update with let and year, cleaner variable names

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,50 @@
 // JS by Dan Høegh
-// UCN MMD 2018
+// UCN MMD 2020
 
-// Wait for the HTML document to be loaded before we start doing stuff
-window.onload = function (){
+let playerName = "";
+while (playerName == "") {
+	playerName = prompt('What is your name?');
+}
 
-	var getPlayerName = "";
-	while (getPlayerName == "") {
-		getPlayerName = prompt('What is your name?');
+if (playerName.toLowerCase() == 'dan') {
+
+	write('Hey! It\'s Dan! Ok, you win.');
+	write('Game over');
+
+} else {
+
+	write('Welcome ' + playerName + ', nice to see you!');
+
+	let pocketItem = "";
+	while (pocketItem == "") {
+
+		pocketItem = prompt(playerName + ', you have something in your pocket, what is it? (single word please)');
+		pocketItem = pocketItem.toLowerCase();
+		if (pocketItem.indexOf(' ') != -1) {
+			pocketItem = "";
+		}
+
 	}
 
-	if (getPlayerName.toLowerCase() == 'dan') {
+	write('You take your ' + pocketItem + ' from your pocket and look at it with a distrusting gaze.');
 
-		write('Hey! It\'s Dan! Ok, you win.');
-		write('Game over');
+	var getDestroy = confirm('You get the urge to destroy your ' + pocketItem + ', do want to do it?');
+	if (getDestroy) {
+
+		write('You throw your ' + pocketItem + ' at a nearby wall and watch it pass right through it! Something\'s not right here!?');
+		write('Cthulhu emerges from the shadows as your blood freezes and your sanity leaves for a long vacation.');
+		write('How a simple ' + pocketItem + ' ended the sweet life of ' + playerName + '.');
 
 	} else {
 
-		write('Welcome ' + getPlayerName + ' nice to see you!');
+		write('You shrug and put your ' + pocketItem + ' back into your pocket.');
+		write('Life continues and you win the lottery the following day.');
 
-		var getItem = "";
-		while (getItem == "") {
-
-			getItem = prompt(getPlayerName + ', you have something in your pocket, what is it? (single word please)');
-			if (getItem.indexOf(' ') != -1) {
-				getItem = "";
-			}
-
-		}
-
-		write('You take your ' + getItem + ' from your pocket and look at it with a distrusting gaze.');
-
-		var getDestroy = confirm('You get the urge to destroy your ' + getItem + ', do want to do it?');
-		if (getDestroy) {
-
-			write('You throw your ' + getItem + ' at a nearby wall and watch it pass right through it! Something\'s not right here!?');
-			write('Cthulhu emerges from the shadows as your blood freezes and your sanity leaves for a long vacation.');
-			write('How a simple ' + getItem + ' ended the sweet life of ' + getPlayerName + '.');
-
-		} else {
-
-			write('You shrug and put your ' + getItem + ' back into your pocket.');
-			write('Life continues and you win the lottery the following day.');
-			
-		}
 	}
 }
 
 // Just a function to make output easy, nevermind this
-function write(text){
+function write(text) {
 	var outputDiv = document.getElementById('output');
 	outputDiv.innerHTML = outputDiv.innerHTML + '<p>' + text + '</p>';
 }
